e8ea64410ce7217aa15d57651d92e1a229e95fcd
saladnoob <salad.at.discord@gmail.com>
2024-06-14 22:51:38 +0800

    Delete netlify/functions/test.js

--- netlify/functions/test.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-  console.log('Test function triggered');
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Test function is working!' }),
-  };
-};
